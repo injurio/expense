2ffa675fdb8ba4f93bb356dad579ca9dc0fac5a5
injurio <49044584+injurio@users.noreply.github.com>
2023-12-09 14:00:48 +0700

    initial commit

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhoP-ACIuC5v_4drzEzLxSWhZueqk4bjA",
+  authDomain: "iccs340-8e36f.firebaseapp.com",
+  projectId: "iccs340-8e36f",
+  storageBucket: "iccs340-8e36f.appspot.com",
+  messagingSenderId: "591462050748",
+  appId: "1:591462050748:web:1112330f2bbd04f2a319b9"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
